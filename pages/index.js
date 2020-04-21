0b70a84c750e18f57ced0f36a9a7594f19e9c554
Samuel Otterman <sjotterman@gmail.com>
2020-04-21 14:45:10 -0400

    format with prettier

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
-import Layout from '../components/layout'
-import { useFetchUser } from '../lib/user'
+import Layout from "../components/layout";
+import { useFetchUser } from "../lib/user";
 
 function Home() {
-  const { user, loading } = useFetchUser()
+  const { user, loading } = useFetchUser();
 
   return (
     <Layout user={user} loading={loading}>
@@ -18,7 +18,7 @@ function Home() {
             To test the login click in <i>Login</i>
           </p>
           <p>
-            Once you have logged in you should be able to click in{' '}
+            Once you have logged in you should be able to click in{" "}
             <i>Profile</i> and <i>Logout</i>
           </p>
         </>
@@ -27,13 +27,12 @@ function Home() {
       {user && (
         <>
           <h4>Rendered user info on the client</h4>
-          <img src={user.picture} alt="user picture" />
           <p>nickname: {user.nickname}</p>
           <p>name: {user.name}</p>
         </>
       )}
     </Layout>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- pages/profile.js
+++ pages/profile.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 // This import is only needed when checking authentication status directly from getInitialProps
 // import auth0 from '../lib/auth0'
-import { useFetchUser } from '../lib/user'
-import Layout from '../components/layout'
+import { useFetchUser } from "../lib/user";
+import Layout from "../components/layout";
 
 function ProfileCard({ user }) {
   return (
@@ -17,17 +17,17 @@ function ProfileCard({ user }) {
         <p>name: {user.name}</p>
       </div>
     </>
-  )
+  );
 }
 
 function Profile() {
-  const { user, loading } = useFetchUser({ required: true })
+  const { user, loading } = useFetchUser({ required: true });
 
   return (
     <Layout user={user} loading={loading}>
       {loading ? <>Loading...</> : <ProfileCard user={user} />}
     </Layout>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
