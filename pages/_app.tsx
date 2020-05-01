b0841d04f57c88f4fa3568ed021a32f5b3249504
Samuel Otterman <sjotterman@gmail.com>
2020-05-01 09:34:07 -0400

    redux MVP

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import "semantic-ui-css/semantic.min.css";
-// Not entirely sure why I need this
-// eslint-disable-next-line no-unused-vars
-import { AppProps } from "next/app";
-
-// This default export is required in a new `pages/_app.js` file.
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
