9cb31482958cb1089aad86b557072c7b019f1c36
Samuel Otterman <sjotterman@gmail.com>
2020-05-01 11:16:11 -0400

    convert counter to use functional component

--- pages/counter.js
+++ pages/counter.js
@@ -1,35 +1,22 @@
 import React from "react";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import {
   decrementCounter,
   incrementCounter,
 } from "../redux/actions/counterActions";
 
-class App extends React.Component {
-  static getInitialProps({ store }) {}
+const App = () => {
+  const counter = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <button onClick={() => dispatch(incrementCounter())}>Increment</button>
+      <button onClick={() => dispatch(decrementCounter())}>Decrement</button>
 
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.props.incrementCounter}>Increment</button>
-        <button onClick={this.props.decrementCounter}>Decrement</button>
-        <h1>{this.props.counter}</h1>
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state) => ({
-  counter: state.counter.value,
-});
-
-const mapDispatchToProps = {
-  incrementCounter: incrementCounter,
-  decrementCounter: decrementCounter,
+      <h1>{counter}</h1>
+    </div>
+  );
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// App.getInitialProps = async (ctx) => {};
+export default App;
